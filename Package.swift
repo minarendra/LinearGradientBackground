b5f6b492a249f923021936c042d0322bb533b2da
minarendra <narendra.singh@mindinventory.com>
2024-02-21 14:06:12 +0530

    - initial commit

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibrary",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,9 +16,6 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "MyLibrary"),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+            name: "MyLibrary")
     ]
 )
--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,29 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Swift
+
+public struct LinearGradientBackground: ViewModifier {
+    let gradient: LinearGradient
+    let opacity: Double
+    public func body(content: Content) -> some View {
+        content.background(
+            gradient
+                .opacity(opacity)
+                .edgesIgnoringSafeArea(.all)
+        )
+    }
+}
+
+
+extension View {
+/// Generic background function for scenes
+    func linearGradientBackground(_ gradient: LinearGradient = LinearGradient(colors: [.red, .pink], startPoint: .top, endPoint: .bottom), _ opacity: Double = 1.0) -> some View {
+        self
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+            .modifier(LinearGradientBackground(gradient: gradient, opacity: opacity))
+    }
+}
+
+
