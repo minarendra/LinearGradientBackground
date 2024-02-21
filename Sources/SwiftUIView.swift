0810130707fd27c8e49061d50ae228fe64e63d79
minarendra <narendra.singh@mindinventory.com>
2024-02-21 15:06:08 +0530

    - Added SwiftUI view

--- Sources/SwiftUIView.swift
+++ Sources/SwiftUIView.swift
@@ -7,9 +7,11 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct SwiftUIView: View {
+   public var body: some View {
+        Text("Hello, World!")
+            .font(.title)
+       Text("This is from Swift Package Manager!!!!")
     }
 }
 
