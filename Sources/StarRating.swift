60ed42bf913a1d6f1e0cb66da620b10203a0791b
minarendra <narendra.singh@mindinventory.com>
2024-02-22 12:10:47 +0530

    - Set custom height for rating view

--- Sources/StarRating.swift
+++ Sources/StarRating.swift
@@ -7,35 +7,26 @@
 
 import SwiftUI
 
-struct StarRating: View {
+public struct StarRating: View {
     // MARK: - Variables & Constant
     @Binding public var rating: Int
     @State private var dragValue: DragGesture.Value?
     @State public var fillImage: String = ""
     @State public var normalImage: String = ""
-    let maxRating: Double = 5
+    public var height: CGFloat = 45
+   private let maxRating: Double = 5
     // MARK: - View Main Body
-    
-    var body: some View {
+    public var body: some View {
         HStack {
             ForEach(1...5, id: \.self) { index in
                 Image(systemName: index <= rating ? "star.fill" : "star")
+                    .resizable()
+                    .scaledToFit()
                     .foregroundColor(.yellow)
+                    .frame(height: height)
                     .onTapGesture {
                         rating = index
                     }
-                    .gesture(DragGesture().onChanged { value in
-                        let width = UIScreen.main.bounds.width / CGFloat(self.maxRating)
-                        let newRating = min(max(0, (value.translation.width + width / 2) / width), self.maxRating)
-                        self.rating = Int(newRating)
-                    }
-                        .onEnded({ value in
-                            let width = UIScreen.main.bounds.width / CGFloat(self.maxRating)
-                            let newRating = min(max(0, (value.translation.width + width / 2) / width), self.maxRating)
-                            self.rating = Int(newRating)
-                            print("newRating: \(newRating)")
-                        })
-                    )
             }
         }
     }
