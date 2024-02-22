8f5f10fb45cec8d3e552215020bef2c513f08053
minarendra <narendra.singh@mindinventory.com>
2024-02-22 16:15:54 +0530

    - Add Snap kit package dependence

--- Package.swift
+++ Package.swift
@@ -12,11 +12,20 @@ let package = Package(
             name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
+    dependencies: [
+            // Add your package dependencies here.
+            .package(url: "https://github.com/SnapKit/SnapKit", from: "5.7.1"),
+        ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MyLibrary",
-            path: "Sources")
+            dependencies: ["SnapKit"],
+            path: "Sources",
+            resources: [
+                .process("Resources/Media.xcassets")
+            ]
+        )
     ]
 )
